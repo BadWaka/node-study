c00d4e647a2c4a4703504672ccaa18bd48c88081
BadWaka <1456683844@qq.com>
2017-06-01 23:14:28 +0800

    4%

---
+++ server.js
@@ -0,0 +1,15 @@
+var http = require('http');
+var querystring = require('querystring');
+
+http.createServer(function (req, res) {
+    var postData = '';
+    req.setEncoding('utf8');
+    req.on('data', function (chunk) {
+        postData += chunk;
+    });
+    req.on('end', function () {
+        res.end(postData);
+    });
+}).listen(8080);
+
+console.log('服务启动完成');
